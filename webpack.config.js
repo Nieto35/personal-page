a72e91b8570c2b311bb974518ff74edc08d22233
Nieto <pi_1999-t@hotmail.com>
2022-10-30 00:17:38 +0200

    otra vez

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "build"),
     filename: "bundle.js",
-    publicPath: "personal-page/",
+    publicPath: "./",
   },
   resolve: {
     extensions: [".js", ".jsx"],
