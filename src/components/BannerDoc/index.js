909aafcf75089405e868b31ab98c55b80aaf55ee
Nieto <pi_1999-t@hotmail.com>
2022-10-29 18:28:06 +0200

    finish

--- src/components/BannerDoc/index.js
+++ src/components/BannerDoc/index.js
@@ -5,12 +5,14 @@ import "animate.css";
 import "@styles/Projects.css";
 // CLOSE STYLES
 // IMAGES
-import gitHubSearch from "@images/gitHub-Search.png";
-import gitHubImage from "@images/gitHub.png";
-import gitHubParzibyte from "@images/gitHub-parzibyte.png";
-import urlApi from "@images/url-api.png";
-import buscadorApi from "@images/buscadorApi.png";
-import error404 from "@images/error-404.png";
+import middleware from "@images/middleware.png";
+import rootReducer from "@images/rootReducer.png";
+import slices from "@images/Slices.png";
+import slices2 from "@images/Slices-2.png";
+import folder from "@images/folder.png";
+import devdependencies from "@images/devdependencies.png";
+import dependencias from "@images/dependencias.png";
+import api from "@images/Api.png";
 // CLOSE IMAGES
 import TrackVisibility from "react-on-screen";
 // IMPORT CONTEXT
@@ -19,8 +21,8 @@ import AppContext from "@appContext";
 
 // LANGUAGE
 import { IntlProvider, FormattedMessage } from "react-intl";
-import docApiEnglish from "@constants/DocApi/en-US.json";
-import docApiSpanish from "@constants/DocApi/es-ES.json";
+import bannerDocEnglish from "@constants/BannerDoc/en-US.json";
+import bannerDocSpanish from "@constants/BannerDoc/es-ES.json";
 // CLOSE LANGUAGE
 
 const BannerDoc = () => {
@@ -29,7 +31,7 @@ const BannerDoc = () => {
   return (
     <IntlProvider
       locale={!!languages[0].state ? "es-ES" : "en-EN"}
-      messages={!!languages[0].state ? docApiSpanish : docApiEnglish}
+      messages={!!languages[0].state ? bannerDocSpanish : bannerDocEnglish}
     >
       <section className={darkMode ? "project dark" : "project"} id="projects">
         <Container>
@@ -44,46 +46,184 @@ const BannerDoc = () => {
                   >
                     <h2>
                       <FormattedMessage
-                        id="docApi.Title"
-                        defaultMessage="How to use the API?"
+                        id="BannerDoc.Title"
+                        defaultMessage="How is this page programmed?"
                       />
                     </h2>
                     <br></br>
+                    <Row>
+                      <Col xl={4}>
+                        <div>
+                          <p>
+                            <FormattedMessage
+                              id="BannerDoc.subTitle"
+                              defaultMessage="How is this page programmed?"
+                            />
+                          </p>
+                        </div>
+                        <Row className="justify-content-center">
+                          <Col xl={8}>
+                            <img
+                              className="img-api"
+                              src={dependencias}
+                              alt="image gitHub"
+                            />
+                          </Col>
+                        </Row>
+                      </Col>
+                      <Col xl={4}>
+                        <div>
+                          <p>
+                            <FormattedMessage
+                              id="BannerDoc.subTitle.1"
+                              defaultMessage="How is this page programmed?"
+                            />
+                          </p>
+                        </div>
+                        <Row className="justify-content-center">
+                          <Col xl={8}>
+                            <img
+                              className="img-api"
+                              src={devdependencies}
+                              alt="image gitHub"
+                            />
+                          </Col>
+                        </Row>
+                      </Col>
+                      <Col xl={4}>
+                        <div>
+                          <p>
+                            <FormattedMessage
+                              id="BannerDoc.subTitle.2"
+                              defaultMessage="How is this page programmed?"
+                            />
+                          </p>
+                        </div>
+                        <Row className="justify-content-center">
+                          <Col xl={8}>
+                            <img
+                              className="img-api"
+                              src={folder}
+                              alt="image gitHub"
+                            />
+                          </Col>
+                        </Row>
+                      </Col>
+                    </Row>
+
+                    <br></br>
+
+                    <h2>
+                      <FormattedMessage
+                        id="BannerDoc.Title.1"
+                        defaultMessage="How is this page programmed?"
+                      />
+                    </h2>
+                    <br></br>
+                    <Row>
+                      <Col xl={4}>
+                        <div>
+                          <p>Slices</p>
+                        </div>
+                        <Row className="justify-content-center">
+                          <Col xl={8}>
+                            <img
+                              className="img-api"
+                              src={slices}
+                              alt="image gitHub"
+                            />
+                          </Col>
+                        </Row>
+                        <div>
+                          <p>
+                            <FormattedMessage
+                              id="BannerDoc.p.1"
+                              defaultMessage="How is this page programmed?"
+                            />
+                          </p>
+                        </div>
+                        <Row className="justify-content-center">
+                          <Col xl={8}>
+                            <img
+                              className="img-api"
+                              src={slices2}
+                              alt="image gitHub"
+                            />
+                          </Col>
+                        </Row>
+                      </Col>
+                      <Col xl={4}>
+                        <div>
+                          <p>redux root</p>
+                        </div>
+                        <Row className="justify-content-center">
+                          <Col xl={8}>
+                            <img
+                              className="img-api"
+                              src={rootReducer}
+                              alt="image gitHub"
+                            />
+                          </Col>
+                        </Row>
+                        <div>
+                          <p>
+                            <FormattedMessage
+                              id="BannerDoc.p.2"
+                              defaultMessage="How is this page programmed?"
+                            />
+                          </p>
+                        </div>
+                      </Col>
+                      <Col xl={4}>
+                        <div>
+                          <p>middleware</p>
+                        </div>
+                        <Row className="justify-content-center">
+                          <Col xl={8}>
+                            <img
+                              className="img-api"
+                              src={middleware}
+                              alt="image gitHub"
+                            />
+                          </Col>
+                        </Row>
+                        <div>
+                          <p>
+                            <FormattedMessage
+                              id="BannerDoc.p.3"
+                              defaultMessage="How is this page programmed?"
+                            />{" "}
+                            <a href="https://chrome.google.com/webstore/detail/redux-devtools/lmhkpmbekcpmknklioeibfkpmmfibljd?hl=es">
+                              React devTools
+                            </a>
+                          </p>
+                        </div>
+                        <Row className="justify-content-center">
+                          <Col xl={8}>
+                            <img
+                              className="img-api"
+                              src={api}
+                              alt="image gitHub"
+                            />
+                          </Col>
+                        </Row>
+                      </Col>
+                    </Row>
+
+                    <br></br>
+
+                    <h2>
+                      <FormattedMessage
+                        id="BannerDoc.Title.2"
+                        defaultMessage="How is this page programmed?"
+                      />
+                    </h2>
+                    <br></br>
+
                     <div>
-                      <p>hola empieza a escribir aqui</p>
-                    </div>
-                    <div>
-                      <p>hola empieza a escribir aqui</p>
-                    </div>
-                    <div>
-                      <p>hola empieza a escribir aqui</p>
-                    </div>
-                    <div>
-                      <p>hola empieza a escribir aqui</p>
-                    </div>
-                    <div>
-                      <p>hola empieza a escribir aqui</p>
-                    </div>
-                    <div>
-                      <p>hola empieza a escribir aqui</p>
-                    </div>
-                    <div>
-                      <p>hola empieza a escribir aqui</p>
-                    </div>
-                    <div>
-                      <p>hola empieza a escribir aqui</p>
-                    </div>
-                    <div>
-                      <p>hola empieza a escribir aqui</p>
-                    </div>
-                    <div>
-                      <p>hola empieza a escribir aqui</p>
-                    </div>
-                    <div>
-                      <p>hola empieza a escribir aqui</p>
-                    </div>
-                    <div>
-                      <p>hola empieza a escribir aqui</p>
+                      <p>Webpack</p>
+                      <p>Mailchimp</p>
+                      <p>Emailjs</p>
                     </div>
                   </div>
                 )}
--- src/reducer/rootReducer/index.js
+++ src/reducer/rootReducer/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from "redux";
-// import { userReducer } from "@reducer/user";
 import dataReducer from "@slices/dataSlice";
 import loadingSlice from "@slices/loadingSlice";
 
 const rootReducer = combineReducers({
   data: dataReducer,
   loading: loadingSlice,
-  // loading: loadingReducer,
 });
 
 export default rootReducer;
